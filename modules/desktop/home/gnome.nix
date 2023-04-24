a686b11f17b725cc3af722b7206399765d04340e
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2023-04-24 21:17:56 +0100

    Defined some default gnome settings for my user
    Defined dark theme, tap-to-click, numlock on by default, and reordered some quick launch apps

--- modules/desktop/home/gnome.nix
+++ modules/desktop/home/gnome.nix
@@ -5,11 +5,21 @@
         "org.gnome.Console.desktop"
         "code.desktop"
         "firefox.desktop"
-        "org.telegram.desktop.desktop"
-        "org.gnome.TextEditor.desktop"
         "org.gnome.Nautilus.desktop"
+        "org.gnome.TextEditor.desktop"
         "bitwarden.desktop"
+        "org.telegram.desktop.desktop"
       ];
+    };
+    "org/gnome/desktop/peripherals/keyboard" = {
+      numlock-state = true; # numlock on by default
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true; # tap to click the button
+      two-finger-scrolling-enabled = true; # scroll with two fingers
+    };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark"; # dark theme
     };
   };
 }
