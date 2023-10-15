29f01a69b02d884ac726406d35c94b50175d78c2
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2023-10-15 23:09:16 +0100

    Updated flakes and hardware-configuration
    This repo no longer needs a dev-shell
    Also updated the hardware-configuration.nix for the Acer machine

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    utils.url = "github:numtide/flake-utils";
+    # utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       # url = "github:nix-community/home-manager/release-23.05"; # for nixos stable
@@ -14,23 +14,12 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, utils, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       user = "vinicius";
     in
     {
       nixosConfigurations = (import ./hosts { inherit nixpkgs user home-manager; });
       templates = (import ./templates);
-    } // utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            unzip # required for scripts/update-vscode-extensions
-          ];
-          NIXPKGS_ROOT = nixpkgs;
-        };
-      });
+    };
 }
--- hosts/acer/hardware-configuration.nix
+++ hosts/acer/hardware-configuration.nix
@@ -5,11 +5,10 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
