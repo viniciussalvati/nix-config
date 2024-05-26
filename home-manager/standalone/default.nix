2f36a1a17f4322fc85204283457299fd57e7cd67
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-05-26 01:12:35 +0100

    Updated the format of the common config

--- home-manager/standalone/default.nix
+++ home-manager/standalone/default.nix
@@ -1,29 +1,24 @@
 # specific configuration for standalone home-manager
 { lib, unstablePkgs, config, ... }:
-lib.mkIfStandalone config
-  (
-    let
-      standaloneCheats = unstablePkgs.writeTextDir "home-manager.cheat" ''
-        % home-manager
+lib.mkIfStandalone config (let
+  standaloneCheats = unstablePkgs.writeTextDir "home-manager.cheat" ''
+    % home-manager
 
-        # home-manager switch for '${config.common.hostname}' --- you MUST be in the nix-config directory
-        nix run '.?submodules=1#homeConfigurations.${config.common.hostname}.activationPackage'
-      '';
-    in
-    {
-      nixpkgs.config.allowUnfree = true;
-      nix = {
-        package = unstablePkgs.nixVersions.git;
-        # Allows me to use nix flakes
-        extraOptions = "experimental-features = nix-command flakes";
-        checkConfig = false; # This is temporary, as it seems home-manager can't build without it for now
-      };
-      # home.packages = [ unstablePkgs.nixVersions.unstable ];
+    # home-manager switch for '${config.hostname}' --- you MUST be in the nix-config directory
+    nix run '.?submodules=1#homeConfigurations.${config.hostname}.activationPackage'
+  '';
+in {
+  nixpkgs.config.allowUnfree = true;
+  nix = {
+    package = unstablePkgs.nixVersions.git;
+    # Allows me to use nix flakes
+    extraOptions = "experimental-features = nix-command flakes";
+    checkConfig =
+      false; # This is temporary, as it seems home-manager can't build without it for now
+  };
+  # home.packages = [ unstablePkgs.nixVersions.unstable ];
 
-      programs.zsh.initExtraFirst = builtins.readFile ./zsh-init-extra-first.zsh;
+  programs.zsh.initExtraFirst = builtins.readFile ./zsh-init-extra-first.zsh;
 
-      programs.navi.settings.cheats.paths = [
-        standaloneCheats
-      ];
-    }
-  )
+  programs.navi.settings.cheats.paths = [ standaloneCheats ];
+})
--- options/common.nix
+++ options/common.nix
@@ -1,11 +1,20 @@
-{ lib, ... }:
-{
-  options.common = {
-    hostname = lib.mkOption
-      {
-        description = "The hostname set in flake for the current nixos system or standalone home-manager installation";
-        type = lib.types.str;
-        readOnly = true;
-      };
+{ lib, ... }: {
+  options = {
+    hostname = lib.mkOption {
+      description =
+        "The hostname set in flake for the current nixos system or standalone home-manager installation";
+      type = lib.types.str;
+      readOnly = true;
+    };
+    username = lib.mkOption {
+      description = "The name of the default user";
+      type = lib.types.str;
+      readOnly = true;
+    };
+    homeDirectory = lib.mkOption {
+      description = "The path of the home directory";
+      type = lib.types.str;
+      readOnly = true;
+    };
   };
 }
