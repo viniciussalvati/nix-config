2c1b42626670edfaa9fecf6dc592dc5036e2a9fc
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-05-27 18:59:29 +0100

    Added Steam utilities
    - Added `gamescope` to improve games and fix issues
    - Added `mangohud` to add hud for me to get an overview of system status (CPU load and temp, GPU load and temp, memory usage, etc)
    - Added `protonup` to install proton, as well as a env to install proton on

--- nixos/games/steam.nix
+++ nixos/games/steam.nix
@@ -1,7 +1,25 @@
-{
+{ pkgs, config, ... }: {
+  # Enables support for the steam controller
   hardware.steam-hardware.enable = true;
   programs.steam = {
     enable = true;
+    # Allows starting games with a micro-compositor to improve games and fix issues
+    gamescopeSession.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    # Program to monitor systems status (CPU and GPU load and temp, RAM, FPS, etc)
+    mangohud
+    protonup
+  ];
+
+  # Improves game performance by temporarily applying optimizations to the OS and game process
   programs.gamemode.enable = true;
+
+  environment.sessionVariables = {
+    # Install proton with
+    # protonup
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "${config.homeDirectory}/.steam/root/compatibilitytools.d";
+  };
 }
