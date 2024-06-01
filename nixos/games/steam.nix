858fb97cf7842c3c86e02ee04ecda6c2fcf5d66a
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-06-01 19:10:06 +0100

    Updated Steam to use the latest version

--- nixos/games/steam.nix
+++ nixos/games/steam.nix
@@ -1,13 +1,14 @@
-{ pkgs, config, lib, ... }:
+{ unstablePkgs, config, lib, ... }:
 lib.mkIf config.programs.steam.enable {
   # Enables support for the steam controller
   hardware.steam-hardware.enable = true;
   programs.steam = {
+    package = unstablePkgs.steam;
     # Allows starting games with a micro-compositor to improve games and fix issues
     gamescopeSession.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with unstablePkgs; [
     # Program to monitor systems status (CPU and GPU load and temp, RAM, FPS, etc)
     mangohud
     protonup
