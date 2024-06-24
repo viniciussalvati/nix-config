f6746a621b0789bdb8537490ea8da15c3bc8ab5a
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-06-24 20:59:22 +0100

    Another update to stylix
    Set the stylix.polarity to ensure I would always get a dark theme

--- shared/stylix/default.nix
+++ shared/stylix/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   stylix.enable = true;
+  stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
   stylix.image = ./wallpaper.jpeg;
 
