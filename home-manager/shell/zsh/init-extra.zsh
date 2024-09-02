cdd940788be0dfb05245e18e6cff16a9a1e96596
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-09-02 01:05:39 +0100

    Added a second shortcut to navi
    This shortcut will allow me to start navi without delay

--- home-manager/shell/zsh/init-extra.zsh
+++ home-manager/shell/zsh/init-extra.zsh
@@ -11,7 +11,7 @@ zle -N clear-screen-and-scrollback
 bindkey '^L' clear-screen-and-scrollback
 # End shortcut to reset the terminal
 
-# Function that runs everytime the prompt will be rendered
+# Function that runs every time the prompt will be rendered
 precmd() {
   # Prints a special command to the terminal so it can duplicate the tab in the same path
   printf "\e]9;9;%s\e\\" $PWD
@@ -30,4 +30,8 @@ zstyle ':completion:*:*:docker-*:*' option-stacking yes
 # Sets Ctrl+arrow keys to move cursor across words
 bindkey ';5C' forward-word
 bindkey ';5D' backward-word
+
+# Adds a new key binding to navi widget
+# This is because Ctrl+G alone was being delayed by fzf-git
+bindkey '^g^g' _navi_widget
 
