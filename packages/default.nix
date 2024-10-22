91252d8aa4fc0ab9fbce885237e3a8df90915142
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-10-22 11:36:14 +0100

    Use BrowserStackLocal flake I've created
    Replaced the local BrowserStackLocal package with the one in the flake.

--- packages/default.nix
+++ packages/default.nix
@@ -1,12 +1,11 @@
 inputs:
 let
   inherit (import ../lib inputs) deepMerge;
-  BrowserStackLocal = import ./BrowserStackLocal inputs;
   nixvim = import ./nixvim inputs;
   git-fuzzy = import ./git-fuzzy.nix inputs;
 in
 deepMerge [
-  BrowserStackLocal
   nixvim
   git-fuzzy
+  inputs.browser-stack-local-flake
 ]
