0f2e7da72b70870117b1e3ae445cc9022f8248c5
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-11-11 14:09:14 +0000

    Updated some treesitter settings

--- packages/nixvim/config/plugins/treesitter.nix
+++ packages/nixvim/config/plugins/treesitter.nix
@@ -1,15 +1,18 @@
 {
   plugins.treesitter = {
     enable = true;
-    settings.indent.enable = true;
     nixGrammars = true; # Because of this, we won't need ensureInstalled, since all the packages we may need comes from nixpkgs
-    settings.incremental_selection = {
-      enable = true;
-      keymaps = {
-        init_selection = "<C-space>";
-        node_incremental = "<C-space>";
-        scope_incremental = "<C-s>";
-        node_decremental = "<C-backspace>";
+    settings = {
+      highlight.enable = true;
+      indent.enable = true;
+      incremental_selection = {
+        enable = true;
+        keymaps = {
+          init_selection = "<C-space>";
+          node_incremental = "<C-space>";
+          scope_incremental = "<C-s>";
+          node_decremental = "<C-backspace>";
+        };
       };
     };
   };
