d47fef2ad61a960d52337eb17a1e16bf038b8d4f
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-12-01 18:25:36 +0000

    Updated flake references for nixos 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Configurations for my nixos machines";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lanzaboote = {
@@ -15,13 +15,14 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05"; # for nixos stable
+      url = "github:nix-community/home-manager/release-24.11"; # for nixos stable
       # url = "github:nix-community/home-manager"; # for nixos unstable
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     stylix = {
-      url = "github:danth/stylix/release-24.05";
+      url = "github:danth/stylix";
+      # url = "github:danth/stylix/release-24.05";
       inputs.home-manager.follows = "home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
