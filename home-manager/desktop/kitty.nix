b532b04d5de5de176148e57498acf1e2968441db
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-12-03 20:22:52 +0000

    Updated some Kitty settings
    - Updated the font to use FiraCode
    - Configured kitty to show the target of a link when I hover it with the mouse

--- home-manager/desktop/kitty.nix
+++ home-manager/desktop/kitty.nix
@@ -1,8 +1,11 @@
-{ unstablePkgs, ... }:
+{ unstablePkgs, lib, ... }:
 {
   programs.kitty = {
     enable = true;
+    font.name = lib.mkForce "FiraCode Nerd Font";
     settings = {
+      show_hyperlink_targets = "yes";
+
       shell = "${unstablePkgs.nushell}/bin/nu";
       editor = "nvim";
     };
