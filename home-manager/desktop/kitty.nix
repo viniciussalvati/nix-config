519e986c5b2a6ee1e2b5e0a6851d22ea3468f0b4
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2024-12-08 01:29:05 +0000

    Updated some kitty configs
    The settings are related to copy&paste shortcuts with mouse

--- home-manager/desktop/kitty.nix
+++ home-manager/desktop/kitty.nix
@@ -4,13 +4,19 @@
     enable = true;
     font.name = lib.mkForce "FiraCode Nerd Font";
     settings = {
-      show_hyperlink_targets = "yes";
-
       shell = "${unstablePkgs.nushell}/bin/nu";
       editor = "nvim";
+
+      copy_on_select = true;
+      show_hyperlink_targets = "yes";
+      strip_trailing_spaces = "smart";
     };
     keybindings = {
       "ctrl+shift+t" = "new_tab_with_cwd";
     };
+    extraConfig = ''
+      mouse_map right press grabbed,ungrabbed no-op
+      mouse_map right click grabbed,ungrabbed paste_from_clipboard
+    '';
   };
 }
