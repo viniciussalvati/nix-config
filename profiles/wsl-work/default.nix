f702cd05c6d9aaca9bea4c567e90b519fb1e9d2b
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2025-01-02 12:26:40 +0000

    Fixed an issue with packages used in the work configuration

--- profiles/wsl-work/default.nix
+++ profiles/wsl-work/default.nix
@@ -1,19 +1,26 @@
 { unstablePkgs, localPkgs, ... }:
-{
-  # Forces node 18 to be installed by the node.nix
+let
+  # Forces node 20 to be installed by the node.nix
   # This is also required for yarn to use the same version of nodejs
-  nixpkgs.overlays = [ (_self: _super: { nodejs = unstablePkgs.nodejs_20; }) ];
+  nodejs = unstablePkgs.nodejs_20;
+in
+{
+  nixpkgs.overlays = [ (_self: _super: { inherit nodejs; }) ];
 
   settings.includePrivate = true;
 
-  home.packages = with unstablePkgs; [
-    nodejs
-    yarn
-    awscli2
-    jre_headless
-    maven # used by slots-admin
-    localPkgs.BrowserStackLocal
-  ];
+  home.packages =
+    [
+      nodejs
+      localPkgs.BrowserStackLocal
+    ]
+    ++ (with unstablePkgs; [
+      # Makes yarn use the specific nodejs version
+      (yarn.override { inherit nodejs; })
+      awscli2
+      jre_headless
+      maven # used by slots-admin
+    ]);
 
   programs.navi.settings.cheats.paths = [
     ./navi-cheats
