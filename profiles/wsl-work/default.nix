a25115e2df4f97e2fd1afb705b12e2e9bc93ab04
Vinicius Salvati Melquiades <1378981+viniciussalvati@users.noreply.github.com>
2025-10-31 17:47:24 +0000

    Updated the nodejs version used by the work profile
    Also updated the flakes

--- profiles/wsl-work/default.nix
+++ profiles/wsl-work/default.nix
@@ -2,24 +2,23 @@
 let
   # Forces node 20 to be installed by the node.nix
   # This is also required for yarn to use the same version of nodejs
-  nodejs = unstablePkgs.nodejs_22;
+  nodejs = unstablePkgs.nodejs_24;
 in
 {
   nixpkgs.overlays = [ (_self: _super: { inherit nodejs; }) ];
 
   settings.includePrivate = true;
 
-  home.packages =
-    [
-      nodejs
-      localPkgs.BrowserStackLocal
-    ]
-    ++ (with unstablePkgs; [
-      (yarn.override { inherit nodejs; }) # Yarn with specific node version, instead of latest
-      awscli2 # Used to load some pre-built docker images
-      jre_headless # Used by wiremock, which is used by wtr-website to run business scenario tests
-      maven # used by slots-admin
-    ]);
+  home.packages = [
+    nodejs
+    localPkgs.BrowserStackLocal
+  ]
+  ++ (with unstablePkgs; [
+    (yarn.override { inherit nodejs; }) # Yarn with specific node version, instead of latest
+    awscli2 # Used to load some pre-built docker images
+    jre_headless # Used by wiremock, which is used by wtr-website to run business scenario tests
+    maven # used by slots-admin
+  ]);
 
   programs.navi.settings.cheats.paths = [
     ./navi-cheats
